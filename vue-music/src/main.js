8a30b71f010ec3d0b934e9300b62472ed3036476
tsy <tsy@tsydeMacBook-Pro.local>
2020-12-03 12:29:54 +0800

    config

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'common/stylus/index.styl';
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -9,4 +11,5 @@ new Vue({
   el: '#app',
   router,
   render: h => h(App)
+
 })
